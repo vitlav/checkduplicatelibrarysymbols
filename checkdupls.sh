5c2d704e21dff7b6880e176236dcf9711cb054e1
Vitaly Lipatov <lav@etersoft.ru>
2017-03-10 02:18:46 +0300

    some improvements

--- checkdupls.sh
+++ checkdupls.sh
@@ -22,8 +22,9 @@ echo "Checking $PRG ..."
 echo -n "Read all libs... "
 LANG=C ldd -r $PRG | grep "not found" > $OB.libs.notfound
 LANG=C ldd -r $PRG | grep "undefined symbol" > $OB.libs.undefined
-LANG=C ldd -r $PRG | grep -v "not found" | grep -v "undefined symbol" | sed -e "s|.*=> ||g" | sed -e "s| .*||g" > $OB.libs
-cat $OB.libs | xargs readlink -f > $OB.libs.u
+LANG=C ldd -r $PRG | grep "statically linked" > $OB.libs.statically
+LANG=C ldd -r $PRG | grep -E -v "(not found|undefined symbol|statically linked)" | sed -e "s|.*=> ||g" | sed -e "s| .*||g" > $OB.libs
+cat $OB.libs | xargs --no-run-if-empty readlink -f > $OB.libs.u
 wc -l < $OB.libs.u
 
 if [ -s "$OB.libs.notfound" ] ; then
@@ -37,7 +38,12 @@ fi
 
 echo -n "Get all symbols... "
 rm -f $OB.out $OB.out.libs
-for i in $(cat $OB.libs.u | grep -v "linux-gate.so.1" | grep -v "/libc-.*.so" | grep -v "linux-vdso.so" | grep -v "libwayland") ; do
+touch $OB.out $OB.out.libs
+
+# libpulsecommon: https://github.com/otcshare/pulseaudio/blob/master/src/Makefile.am https://lists.freedesktop.org/archives/pulseaudio-discuss/2012-March/012961.html
+# libtheora: by some reason includes libtheoraenc, libtheoradec
+# TODO: some libs excludes
+for i in $(cat $OB.libs.u | grep -E -v "(linux-gate.so.1|/libc-.*.so|linux-vdso.so|libwayland|libpulsecommon|libtheora)") ; do
 	#$OBjdump -p $PRG | grep NEEDED
 	LANG=C nm -D $i | grep " T " | sed -e "s|.* T ||g" | uniq | tee -a $OB.out | sed -e "s|$| $i|g" >> $OB.out.libs
 done
@@ -45,7 +51,8 @@ wc -l < $OB.out.libs
 
 echo -n "Get all non uniq symbols... "
 sort < $OB.out | uniq -c | sort -n > $OB.out.l
-grep -v " *1 " < $OB.out.l | grep -v " _fini$" | grep -v " _init$" > $OB.nonuniq
+# TODO: some excludes functions
+grep -v " *1 " < $OB.out.l | grep -E -v " (_fini|_init|libVersionPoint)$" > $OB.nonuniq
 wc -l < $OB.nonuniq
 
 echo "Duplicated symbols:"
@@ -53,7 +60,8 @@ for i in $(sed -e "s|.* ||g" < $OB.nonuniq) ; do
 	grep -- "^$i " $OB.out.libs
 done
 
-[ -s $OB.nonuniq ] && [ -x /usr/bin/eepm ] && echo "$PRG $(eepm qf $PRG) with $(wc -l < $OB.nonuniq) duplicated symbols" >> $OB.found
+[ -s $OB.nonuniq ] && [ -x /usr/bin/eepm ] && echo "$PRG $(eepm --quiet qf $PRG) with $(wc -l < $OB.nonuniq) duplicated symbols" >> $OB.found.dup
+[ -s $OB.libs.undefined ] && [ -x /usr/bin/eepm ] && echo "$PRG $(eepm --quiet qf $PRG) with $(wc -l < $OB.libs.undefined) undefined symbols" >> $OB.found.undef
 
 # error status if there are duplicated symbols
 test ! -s $OB.nonuniq
